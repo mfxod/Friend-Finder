c0d1757536543adc7fd62fab11708e1bf02274ba
mfxod <46651745+mfxod@users.noreply.github.com>
2019-08-02 15:27:03 -0400

    added GET and POST requests

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -1,20 +1,16 @@
 
-// ===== DEPENDENCY =====
-const path = require("path");
-
-
-
 // ===== ROUTES =====
 const friends = require("../data/friends");
 
 module.exports = function (app) {
 
-    app.get("/friends", function (req, res) {
-        res.json(friends)
+    app.get("/api/friends", function (req, res) {
+        return res.json(friends)
     });
 
-    app.post("/friends", function (req, res) {
-        // ???.push(req.body);
-        res.json(friends);
+    app.post("/api/friends", function (req, res) {
+        friends.push(req.body);
     });
-}
+};
+
+// console.log(friends);
