80876ed8f90f57ae7a5508e86e9ede0bc9e60e9b
mfxod <46651745+mfxod@users.noreply.github.com>
2019-08-03 15:51:50 -0400

    gets index of friend match!

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -9,8 +9,76 @@ module.exports = function (app) {
     });
 
     app.post("/api/friends", function (req, res) {
-        friends.push(req.body);
-    });
+        
+        function compare(userAnswers, friendAnswers) {
+            // create new array of differences between user answers and friend answers
+            let newArr = [];
+            for (let i = 0; i <= 9; i++) {
+                const result = Math.abs(userAnswers[i] - friendAnswers[i])
+                newArr.push(result)
+            };
+        
+            // get sum of differences of answers
+            const difference = newArr.reduce(function (accumulator, currentValue) {
+                return accumulator + currentValue
+            });
+        
+            return difference
+        };
+        
+        function getDiffs() {
+            // create new array of differences of answers for testing
+            let diffsArr = [];
+            for (let j = 0; j < friends.length; j++) {
+                // replace userTest with user-supplied answers from survey
+                diffsArr.push(compare(req.body.scores, friends[j].scores));
+            };
+            // console.log(diffsArr);
+            return diffsArr
+        };
+        
+        // find smallest value in diffsArr and use its index to display friend match
+        const matchIndex = getDiffs().indexOf(Math.min(...getDiffs()));
+        // console.log("matchIndex: " + matchIndex);
+        
+        
+        
+        // friends.push(req.body)
+    })
 };
 
-// console.log(friends);
+
+
+// ===== COMPATABILITY LOGIC =====
+// const userTest = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+
+// function compare(userAnswers, friendAnswers) {
+//     // create new array of differences between user answers and friend answers
+//     let newArr = [];
+//     for (let i = 0; i <= 9; i++) {
+//         const result = Math.abs(userAnswers[i] - friendAnswers[i])
+//         newArr.push(result)
+//     };
+
+//     // get sum of differences of answers
+//     const difference = newArr.reduce(function (accumulator, currentValue) {
+//         return accumulator + currentValue
+//     });
+
+//     return difference
+// };
+
+// function getDiffs() {
+//     // create new array of differences of answers for testing
+//     let diffsArr = [];
+//     for (let j = 0; j < friends.length; j++) {
+//         // replace userTest with user-supplied answers from survey
+//         diffsArr.push(compare(userTest, friends[j].scores));
+//     };
+//     // console.log(diffsArr);
+//     return diffsArr
+// };
+
+// // find smallest value in diffsArr and use its index to display friend match
+// const matchIndex = getDiffs().indexOf(Math.min(...getDiffs()));
+// // console.log(matchIndex);
