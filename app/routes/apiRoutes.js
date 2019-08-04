75244f40ebbb60930184e3a48721d778e6dd2232
mfxod <46651745+mfxod@users.noreply.github.com>
2019-08-04 10:29:46 -0400

    compatability logic working

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -18,12 +18,13 @@ module.exports = function (app) {
                 const result = Math.abs(userAnswers[i] - friendAnswers[i])
                 newArr.push(result)
             };
+            // console.log("newArr: " + newArr)
         
             // get sum of differences of answers
             const difference = newArr.reduce(function (accumulator, currentValue) {
                 return accumulator + currentValue
             });
-        
+            // console.log("difference: " + difference)
             return difference
         };
         
@@ -31,7 +32,6 @@ module.exports = function (app) {
             // create new array of differences of answers for testing
             let diffsArr = [];
             for (let j = 0; j < friends.length; j++) {
-                // replace userTest with user-supplied answers from survey
                 diffsArr.push(compare(req.body.scores, friends[j].scores));
             };
             // console.log(diffsArr);
@@ -40,46 +40,10 @@ module.exports = function (app) {
         
         // index of the smallest value in the diffsArr (the most compatible friend)
         const matchIndex = getDiffs().indexOf(Math.min(...getDiffs()));
-        // console.log("matchIndex: " + matchIndex);
+        console.log(matchIndex);
         
         // how do I create a modal?
         
         // friends.push(req.body)
     })
 };
-
-
-
-// ===== COMPATABILITY LOGIC =====
-// const userTest = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
-
-// function compare(userAnswers, friendAnswers) {
-//     // create new array of differences between user answers and friend answers
-//     let newArr = [];
-//     for (let i = 0; i <= 9; i++) {
-//         const result = Math.abs(userAnswers[i] - friendAnswers[i])
-//         newArr.push(result)
-//     };
-
-//     // get sum of differences of answers
-//     const difference = newArr.reduce(function (accumulator, currentValue) {
-//         return accumulator + currentValue
-//     });
-
-//     return difference
-// };
-
-// function getDiffs() {
-//     // create new array of differences of answers for testing
-//     let diffsArr = [];
-//     for (let j = 0; j < friends.length; j++) {
-//         // replace userTest with user-supplied answers from survey
-//         diffsArr.push(compare(userTest, friends[j].scores));
-//     };
-//     // console.log(diffsArr);
-//     return diffsArr
-// };
-
-// // find smallest value in diffsArr and use its index to display friend match
-// const matchIndex = getDiffs().indexOf(Math.min(...getDiffs()));
-// // console.log(matchIndex);
